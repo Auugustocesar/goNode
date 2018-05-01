c4536cbaa1c980dbdc112f5a1562f51c76e78a2a
Augusto <augusto.gonzaga2010@gmail.com>
2018-05-01 16:37:04 -0300

    API REST com envio de email

---
+++ modulo3/Aulas/app/services/mailer.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const nodemailer = require('nodemailer')
+const hbs = require('handlebars')
+const htmlToText = require('html-to-text')
+
+const { host, port, user, pass, templatesPath } = require('../../config/mail')
+
+const transport = nodemailer.createTransport({
+  host,
+  port,
+  auth: { user, pass },
+})
+
+module.exports = ({ template, context, ...options }) => {
+  /**
+   * Mail template
+   */
+  let hbsTemplate
+  if (template) {
+    const file = fs.readFileSync(path.join(templatesPath, `${template}.hbs`), 'utf8')
+    hbsTemplate = hbs.compile(file)(context)
+  }
+
+  const mailhtml = hbsTemplate || options.html
+
+  return transport.sendMail({
+    ...options,
+    html: mailhtml,
+    text: htmlToText.fromString(mailhtml).trim(),
+  })
+}
---
+++ modulo3/Aulas/config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: 'b2298b38eb376fcffba4d05bb4e4674a',
+}
